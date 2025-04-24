18c92e8d13d06a6870e669c5ad6f464f98c1ba2e
Galina <baleva1990@gmail.com>
2025-04-24 23:44:42 +0200

    Made a clickable logo

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import { NavLink, useNavigate } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import './Navigation.css';
 
 const Navigation = (props) => {
@@ -6,10 +6,10 @@ const Navigation = (props) => {
     return (
         <>
             <header className="site-header">
-                <div className="header-first">
-                    <div><i className="fa-solid fa-paw"></i></div>
-                    <div className="logo">Pets.Inst</div>
-                </div>
+                    <Link to='/' className="header-first">
+                        <div><i className="fa-solid fa-paw"></i></div>
+                        <div className="logo">Pets.Inst</div>
+                    </Link>
                 <div className="header-second">
                     <div className="navbar-closed">
                         <i className="fa-solid fa-bars" />
@@ -21,7 +21,7 @@ const Navigation = (props) => {
                                 ?
                                 <>
                                     <li><NavLink id="login" to='/login'>Login</NavLink></li>
-                                    <li><NavLink id="register" to='/register'>Register</NavLink></li>
+                                    <li><NavLink id="register" to='/singup'>Register</NavLink></li>
                                 </>
                                 :
                                 <>
