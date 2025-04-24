c8e67091ab2b5414dd0a331e31d6d52d7685ad8d
Galina <baleva1990@gmail.com>
2025-04-24 23:45:03 +0200

    Changed the location of the Home component

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero/Hero"
+import Top from "../components/Top/Top"
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <Top />
+        </>
+    )
+}
+
+export default Home;
